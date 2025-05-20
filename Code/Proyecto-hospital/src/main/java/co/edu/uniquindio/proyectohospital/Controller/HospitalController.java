6a3f9dfa72b8c1c4987ad8b48473714ca444aab2
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2025-05-19 21:42:35 -0500

    Delete Code/Proyecto-hospital directory

--- Code/Proyecto-hospital/src/main/java/co/edu/uniquindio/proyectohospital/Controller/HospitalController.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.hospital.Controller;
-
-public class HospitalController {
-}
--- Code/Proyecto-hospital/src/main/java/co/edu/uniquindio/proyectohospital/Viewcontroller/HospitalViewController.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.hospital.Viewcontroller;
-
-public class HospitalViewController {
-}
