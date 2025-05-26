a2b0e004acbc40300582267f28480c0b5222ff76
NICO-O2 <nicolayrr2@gmail.com>
2025-05-26 16:59:46 -0500

    Documentacion cita medica

--- Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/CitaMedica.java
+++ Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/CitaMedica.java
@@ -12,31 +12,52 @@ public class CitaMedica {
         this.paciente = paciente;
         this.medico = medico;
     }
-
+    /**
+     * Metodo para retornar la hora
+     * @return
+     */
     public LocalDateTime getFechaHora() {
         return fechaHora;
     }
-
+    /**
+     * metodo para retornar el paciente
+     * @return
+     */
     public Paciente getPaciente() {
         return paciente;
     }
-
+    /**
+     * metodo para retornar al medico
+     * @return
+     */
     public Medico getMedico() {
         return medico;
     }
-
+    /**
+     * Metodo para asignar hora
+     * @param fechaHora
+     */
     public void setFechaHora(LocalDateTime fechaHora) {
         this.fechaHora = fechaHora;
     }
-
+    /**
+     * metodo para asignar paciente
+     * @param paciente
+     */
     public void setPaciente(Paciente paciente) {
         this.paciente = paciente;
     }
-
+    /**
+     * Metodo para asignar medico
+     * @param medico
+     */
     public void setMedico(Medico medico) {
         this.medico = medico;
     }
-
+    /**
+     * Metodo que muestras los detalles asignados de la cita
+     * @return
+     */
     public String detallesCita() {
         return "Cita con el Dr. " + medico.nombre + " para " + paciente.nombre + " el " + fechaHora.toString();
     }
