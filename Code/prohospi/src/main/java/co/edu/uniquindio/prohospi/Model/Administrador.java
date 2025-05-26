c5a9e2018f3905fe1309f365abfc6a6e3dd3f7df
NICO-O2 <nicolayrr2@gmail.com>
2025-05-26 16:39:22 -0500

    Documentacion Administrador

--- Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/Administrador.java
+++ Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/Administrador.java
@@ -11,54 +11,101 @@ public class Administrador extends Persona {
         this.contrasena=contrasena;
         this.usuario=usuario;
     }
-
+    /**
+     * metodo para registrar un paciente
+     * @param paciente
+     */
     public void registrarPaciente(Paciente paciente) {
         System.out.println("Paciente registrado: " + paciente.getNombre());
     }
-
+    /**
+     * Metodo para registrar un medico
+     * @param medico
+     */
     public void registrarMedico(Medico medico) {
         System.out.println("Médico registrado: " + medico.getNombre());
     }
-
+    /**
+     * Metodo para retornar la contraseña
+     * @return
+     */
     public String getContrasena() {
         return contrasena;
     }
-
+    /**
+     * Metodo para asignar la contraseña
+     * @param contrasenia
+     */
     public void setContrasena(String contrasenia) {
         this.contrasena = contrasenia;
     }
-
+    /**
+     * Metodo para asignar el usuario
+     * @param usuario
+     */
     public void setusuario(String usuario) {
         this.usuario = usuario;
     }
-
+    /**
+     * Metodo para retornar el usuario
+     * @return
+     */
     public String getusuario() {
         return usuario;
     }
+     /**
+     * Metodo para registrar medico en el hospital
+     * @param hospital
+     * @param nuevoMedico
+     */
     public void registrarMedico(Hospital hospital, Medico nuevoMedico) {
         hospital.agregarMedico(nuevoMedico);
     }
-
+    /**
+     * Metodo para modificar la especialidad del medico
+     * @param medico
+     * @param nuevaEspecialidad
+     */
     public void modificarMedico(Medico medico, String nuevaEspecialidad) {
         medico.setEspecialidad(nuevaEspecialidad);
     }
-
+    /**
+     * Metodo para eliminar un medico 
+     * @param medicos
+     * @param medico
+     */
     public void eliminarMedico(List<Medico> medicos, Medico medico) {
         medicos.remove(medico);
     }
-
+    /**
+     * Metodo para registrar un paciente
+     * @param hospital
+     * @param nuevoPaciente
+     */
     public void registrarPaciente(Hospital hospital, Paciente nuevoPaciente) {
         hospital.agregarPaciente(nuevoPaciente);
     }
-
+    /**
+     * Metodo para eliminar pacientes
+     * @param pacientes
+     * @param paciente
+     */
     public void eliminarPaciente(List<Paciente> pacientes, Paciente paciente) {
         pacientes.remove(paciente);
     }
-
+    /**
+     * Metodo para asignar una sala
+     * @param salas
+     * @param sala
+     */
     public void asignarSala(List<String> salas, String sala) {
         salas.add(sala);
     }
-
+    /**
+     * metodo para eliminar una sala
+     * @param salas
+     * @param sala
+     */
     public void eliminarSala(List<String> salas, String sala) {
         salas.remove(sala);
     }
@@ -72,7 +119,11 @@ public class Administrador extends Persona {
             m.getHorariosConsulta().forEach(h -> System.out.println("- " + h));
         }
     }
-
+    /**
+     * Metodo para asignar un paciente
+     * @param medico
+     * @param paciente
+     */
     public void asignarPaciente(Medico medico, Paciente paciente) {
         medico.agregarPaciente(paciente);
     }
