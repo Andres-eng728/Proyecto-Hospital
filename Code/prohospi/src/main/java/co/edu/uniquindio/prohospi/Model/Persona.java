4c63bab7868d38cffccb0af42272baad533f0c7f
NICO-O2 <nicolayrr2@gmail.com>
2025-05-26 21:28:55 -0500

    Documentacion persona

--- Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/Persona.java
+++ Code/prohospi/src/main/java/co/edu/uniquindio/prohospi/Model/Persona.java
@@ -10,17 +10,37 @@ public abstract class Persona {
         this.identificacion = identificacion;
         this.correo = correo;
     }
-
+    /**
+     * metodo que retorna nombre
+     * @return
+     */
     public String getNombre() { return nombre; }
+     /**
+     * metodo que retorna identificacion
+     * @return
+     */
     public String getIdentificacion() { return identificacion; }
+        /**
+     * metodo que retorna correo
+     * @return
+     */
     public String getCorreo() { return correo; }
-
+    /**
+     * metodo que asigna el correo
+     * @param correo
+     */
     public void setCorreo(String correo) { this.correo = correo; }
-
+    /**
+     * metodo que asigna el nombre
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * metodo para asignar identificacion
+     * @param identificacion
+     */
     public void setIdentificacion(String identificacion) {
         this.identificacion = identificacion;
     }
